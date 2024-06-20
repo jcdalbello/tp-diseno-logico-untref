db01c01d3f5c57d1bc5596c369472044687d9691
JuanCruzDalBello <98571416+JuanCruzDalBello@users.noreply.github.com>
2024-06-20 18:17:51 -0300

    Update decoder.vhd

--- decoder.vhd
+++ decoder.vhd
@@ -16,20 +16,21 @@ end decoder;
 architecture decoder_architecture of decoder is
 begin
 	with input select                         
-        bus_sel <= "10" when "01",
-                   "00" when "02",
-                   "00" when "03",
-                   "00" when "04",
-                   "01" when "05",
-                   "00" when "10",
-                   "00" when "11",
-                   "00" when "12",
-                   "00" when "13",
-                   "00" when "14",
-                   "00" when "20",
-                   "00" when "21",
-                   "00" when others;
+	        bus_sel <= "10" when "01",
+	                   "00" when "02",
+	                   "00" when "03",
+	                   "00" when "04",
+	                   "01" when "05",
+	                   "00" when "10",
+	                   "00" when "11",
+	                   "00" when "12",
+	                   "00" when "13",
+	                   "00" when "14",
+	                   "00" when "20",
+	                   "00" when "21",
+	                   "00" when others;
 
+	with input select
 		alu_op <= "000" when "01",
                   "000" when "02",
                   "000" when "03",
@@ -43,47 +44,50 @@ begin
                   "001" when "20",
                   "111" when "21",
                   "000" when others;
-                  
-        reg_a_we <= "0" when "01",
-                    "0" when "02",
-                    "0" when "03",
-                    "1" when "04",
-                    "1" when "05",
-                    "0" when "10",
-                    "0" when "11",
-                    "0" when "12",
-                    "0" when "13",
-                    "0" when "14",
-                    "0" when "20",
-                    "0" when "21",
-                    "0" when others;
 
-        out_we <= "0" when "01",
-                  "1" when "02",
-                  "0" when "03",
-                  "0" when "04",
-                  "0" when "05",
-                  "0" when "10",
-                  "0" when "11",
-                  "0" when "12",
-                  "0" when "13",
-                  "0" when "14",
-                  "0" when "20",
-                  "0" when "21",
-                  "0" when others;
+	with input select
+	        reg_a_we <= "0" when "01",
+	                    "0" when "02",
+	                    "0" when "03",
+	                    "1" when "04",
+	                    "1" when "05",
+	                    "0" when "10",
+	                    "0" when "11",
+	                    "0" when "12",
+	                    "0" when "13",
+	                    "0" when "14",
+	                    "0" when "20",
+	                    "0" when "21",
+	                    "0" when others;
 
-        reg_we <= "1" when "01",
-                  "0" when "02",
-                  "1" when "03",
-                  "0" when "04",
-                  "0" when "05",
-                  "1" when "10",
-                  "1" when "11",
-                  "1" when "12",
-                  "1" when "13",
-                  "1" when "14",
-                  "1" when "20",
-                  "1" when "21",
-                  "1" when others;
+	with input select
+	        out_we <= "0" when "01",
+	                  "1" when "02",
+	                  "0" when "03",
+	                  "0" when "04",
+	                  "0" when "05",
+	                  "0" when "10",
+	                  "0" when "11",
+	                  "0" when "12",
+	                  "0" when "13",
+	                  "0" when "14",
+	                  "0" when "20",
+	                  "0" when "21",
+	                  "0" when others;
+
+	with input select
+	        reg_we <= "1" when "01",
+	                  "0" when "02",
+	                  "1" when "03",
+	                  "0" when "04",
+	                  "0" when "05",
+	                  "1" when "10",
+	                  "1" when "11",
+	                  "1" when "12",
+	                  "1" when "13",
+	                  "1" when "14",
+	                  "1" when "20",
+	                  "1" when "21",
+	                  "1" when others;
 	
 end architecture decoder_architecture;
