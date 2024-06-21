3b02e08696faae6e2029dbf834c17fb749c12bcf
unknown <juancruzdalbello17@gmail.com>
2024-06-21 10:53:10 -0300

    update decorder.vhd

--- decoder.vhd
+++ decoder.vhd
@@ -8,9 +8,9 @@ entity decoder is
 		bus_sel  : out std_logic_vector(1 downto 0);
 		alu_op   : out std_logic_vector(2 downto 0);
 		reg_a_we : out std_logic;
-        	out_we   : out std_logic;
-       	 	reg_we   : out std_logic;
-	);
+        out_we   : out std_logic;
+       	reg_we   : out std_logic 
+		);
 end decoder;
 
 architecture decoder_architecture of decoder is
@@ -28,7 +28,7 @@ begin
 	                   "00" when "14",
 	                   "00" when "20",
 	                   "00" when "21",
-	                   else "00";
+	                   "00" when others;
 
 	with input select
 		alu_op <= "000" when "01",
@@ -43,51 +43,51 @@ begin
 	                  "110" when "14",
 	                  "001" when "20",
 	                  "111" when "21",
-	                  else "000";
+	                  "000" when others;
 
 	with input select
-	        reg_a_we <= "0" when "01",
-	                    "0" when "02",
-	                    "0" when "03",
-	                    "1" when "04",
-	                    "1" when "05",
-	                    "0" when "10",
-	                    "0" when "11",
-	                    "0" when "12",
-	                    "0" when "13",
-	                    "0" when "14",
-	                    "0" when "20",
-	                    "0" when "21",
-	                    else "0";
+	        reg_a_we <= '0' when "01",
+	                    '0' when "02",
+	                    '0' when "03",
+	                    '1' when "04",
+	                    '1' when "05",
+	                    '0' when "10",
+	                    '0' when "11",
+	                    '0' when "12",
+	                    '0' when "13",
+	                    '0' when "14",
+	                    '0' when "20",
+	                    '0' when "21",
+	                    '0' when others;
 
 	with input select
-	        out_we <= "0" when "01",
-	                  "1" when "02",
-	                  "0" when "03",
-	                  "0" when "04",
-	                  "0" when "05",
-	                  "0" when "10",
-	                  "0" when "11",
-	                  "0" when "12",
-	                  "0" when "13",
-	                  "0" when "14",
-	                  "0" when "20",
-	                  "0" when "21",
-	                  else "0";
+	        out_we <= '0' when "01",
+	                  '1' when "02",
+	                  '0' when "03",
+	                  '0' when "04",
+	                  '0' when "05",
+	                  '0' when "10",
+	                  '0' when "11",
+	                  '0' when "12",
+	                  '0' when "13",
+	                  '0' when "14",
+	                  '0' when "20",
+	                  '0' when "21",
+	                  '0' when others;
 
 	with input select
-	        reg_we <= "1" when "01",
-	                  "0" when "02",
-	                  "1" when "03",
-	                  "0" when "04",
-	                  "0" when "05",
-	                  "1" when "10",
-	                  "1" when "11",
-	                  "1" when "12",
-	                  "1" when "13",
-	                  "1" when "14",
-	                  "1" when "20",
-	                  "1" when "21",
-	                  else "0";
+	        reg_we <= '1' when "01",
+	                  '0' when "02",
+	                  '1' when "03",
+	                  '0' when "04",
+	                  '0' when "05",
+	                  '1' when "10",
+	                  '1' when "11",
+	                  '1' when "12",
+	                  '1' when "13",
+	                  '1' when "14",
+	                  '1' when "20",
+	                  '1' when "21",
+	                  '0' when others;
 	
 end architecture decoder_architecture;
